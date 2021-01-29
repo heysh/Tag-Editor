e5e5f001cfacaf3cf0a6d5444b2ad3c90d04c07a
Harshil Surendralal <harshilsure+github@gmail.com>
2021-01-29 14:11:29 +0000

    Added comments describing process

--- tagEditorv2.py
+++ tagEditorv2.py
@@ -3,6 +3,29 @@ class TagEditor:
     def __init__(self):
         print("test")
 
+        # 1) get owner details (name and email)
+        #       a) if details don't exist, ask user to enter details and store (json)
+        #
+        # 2) infinite loop (to catch multiple directories)
+        #
+        # 3) get the directory
+        #
+        # 4) get the paths of all the songs in this directory
+        #
+        # 5) set the number of processed songs to 0
+        #
+        # 6) iterate through every song
+        #
+        # 7) create backup of current song
+        #
+        # 8) set owner details on the current song
+        #       a) iTunes owner details (hex)
+        #       b) remaining owner details (name and email)
+        #
+        # 9) increment number of processed songs
+        #
+        # 10) output "finished processing" message
+
 
 if __name__ == '__main__':
     TagEditor()
